ba9ec8479d81f088dc9066b7afa8a4ab3b0a8892
Sam-Neale <sammneale@icloud.com>
2021-07-10 22:26:58 +1000

    dotenv and paths and express config

---
+++ server/index.js
@@ -0,0 +1,17 @@
+//Dependancies
+const express = require('express');
+const btoaData = require('btoa');
+const atobData = require('atob');
+const bcrypt = require('bcrypt');
+const fs = require('fs');
+const path = require('path');
+require('dotenv').config();
+//Paths
+const viewPath = path.join(__dirname, '/../views/');
+//Express App
+const app = express();
+app.set('view engine', 'ejs');
+app.set('view', viewPath);
+app.listen(process.env.port, () => {
+    console.log("Listening on " + process.env.port);
+});
