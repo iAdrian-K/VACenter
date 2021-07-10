98fae20a8911d794aa8ca96415cc341c95e4c409
Sam-Neale <sammneale@icloud.com>
2021-07-10 22:24:03 +1000

    TSConfig and Dependencies

---
+++ server/index.ts
@@ -0,0 +1,7 @@
+//Dependancies
+const express = require('express');
+const btoaData = require('btoa');
+const atobData = require('atob');
+const bcrypt = require('bcrypt');
+const fs = require('fs');
+const path = require('path');
