905f6db68103bb4e30f66b2571788c8f9822febf
teddy9264 <45321696+teddy9264@users.noreply.github.com>
2021-07-11 09:40:01 +1000

    Updates to database

--- server/db.js
+++ server/db.js
@@ -9,16 +9,68 @@ let db = new sqlite3.Database('./database.db', (err) => {
 });
 //@ts-ignore
 function GetUser(username) {
-    return new Promise(resolve => {
+    return new Promise((resolve, error) => {
         db.serialize(() => {
-            db.each(`SELECT * FROM users WHERE username = ?`, (err, row) => {
+            db.each(`SELECT * FROM users WHERE username = ?`, [username], (err, row) => {
                 if (err) {
-                    console.error(err.message);
+                    error(err.message);
                 }
-                console.log(row);
-                console.log(123);
+                resolve(row);
             });
         });
     });
 }
-module.exports = { db, GetUser };
+//@ts-ignore
+function GetUsers() {
+    return new Promise((resolve, error) => {
+        db.serialize(() => {
+            db.all(`SELECT * FROM users`, (err, rows) => {
+                if (err) {
+                    error(err.message);
+                }
+                resolve(rows);
+            });
+        });
+    });
+}
+//@ts-ignore
+function CreateUser(username, rank, admin, password, display, hours, created, llogin, cp, revoked) {
+    db.run(`INSERT INTO users(username, rank, admin, password, display, hours, created, llogin, cp, revoked) 
+            VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`, [username, rank, admin, password, display, hours, created, llogin, cp, revoked], function (err) {
+        return err;
+    });
+}
+//@ts-ignore
+function GetPirep(id) {
+    return new Promise((resolve, error) => {
+        db.serialize(() => {
+            db.each(`SELECT * FROM pireps WHERE id = ?`, [id], (err, row) => {
+                if (err) {
+                    error(err.message);
+                }
+                resolve(row);
+            });
+        });
+    });
+}
+//@ts-ignore
+function GetPireps() {
+    return new Promise((resolve, error) => {
+        db.serialize(() => {
+            db.all(`SELECT * FROM pireps`, (err, rows) => {
+                if (err) {
+                    error(err.message);
+                }
+                resolve(rows);
+            });
+        });
+    });
+}
+//@ts-ignore
+function CreatePirep(username, rank, admin, password, display, hours, created, llogin, cp, revoked) {
+    db.run(`INSERT INTO users(username, rank, admin, password, display, hours, created, llogin, cp, revoked) 
+            VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`, [username, rank, admin, password, display, hours, created, llogin, cp, revoked], function (err) {
+        return err;
+    });
+}
+module.exports = { db, GetUser, GetUsers, CreateUser, GetPirep, GetPireps, CreatePirep };
