6d115fc19b358dc6ab33062c7e7dab2a60bbf80a
Sam-Neale <sammneale@icloud.com>
2021-07-10 16:35:43 +1000

    Fix errors

--- server/index.js
+++ server/index.js
@@ -4,16 +4,23 @@ function newError(error, title) {
     const fsSPECIAL = require('fs');
     const pathSPECIAL = require('path');
     let config = JSON.parse(fsSPECIAL.readFileSync(pathSPECIAL.join(__dirname, "/../") + "config.json"))
+    let errorB;
+    if(error instanceof Error){
+        errorB = error.toString()
+    }else if(typeof error == "object"){
+        errorB = JSON.stringify(error)
+    }else{
+        errorB = error;
+    }
     const options2 = {
         method: 'POST',
         url: 'https://error.va-center.com/api/reportBug',
-        form: { title: title ? title : "AUTO - ERROR - " + config.name, body: JSON.stringify(error), contact: JSON.stringify(config) }
+        form: { title: title ? title : "AUTO - ERROR - " + config.name, body: errorB, contact: JSON.stringify(config) }
     };
 
     requestSPECIAL(options2, function (error2, response2, body2) {
-        console.log(error2)
-        console.log(response2)
-        console.log(body2)
+        console.log("NEW REPORT")
+        console.log(options2.form)
     })
 }
 
