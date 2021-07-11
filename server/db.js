857d99cb917ce83988ecf9aa4c8f1fe8406bfd3f
teddy9264 <45321696+teddy9264@users.noreply.github.com>
2021-07-11 11:36:13 +1000

    Update db.js

--- server/db.js
+++ server/db.js
@@ -9,12 +9,92 @@ let db = new sqlite3.Database('./database.db', (err) => {
     }
     console.log('Connected to the database.');
 });
+/** @namespace Aircrafts */
 
-//Users
+/** @namespace Events */
+
+/** @namespace Gates */
+
+/** @namespace News */
+
+/** @namespace Notifications */
+
+/** @namespace Operators */
+
+/** @namespace PIREPS */
 /**
- * @desc Returns record of specific UID
- * @param {string} username - unique username of user 
+ * @desc Returns record of specific PIREP id
+ * @param {string} id - Unique id of prirep 
+ * @returns {promise} Record for that prirep in an array
+ * @memberof PIREPS
+ */
+ function GetPirep(id) {
+    return new Promise((resolve, error) => {
+        db.serialize(() => {
+            db.each(`SELECT * FROM pireps WHERE id = ?`, [id], (err, row) => {
+                if (err) {
+                    error(err.message);
+                }
+                resolve(row);
+            });
+        });
+    });
+}
+/**
+ * @desc Returns all pireps
+ * @returns {promise} All pireps in an array
+ * @memberof PIREPS
+ */
+function GetPireps() {
+    return new Promise((resolve, error) => {
+        db.serialize(() => {
+            db.all(`SELECT * FROM pireps`, (err, rows) => {
+                if (err) {
+                    error(err.message);
+                }
+                resolve(rows);
+            });
+        });
+    });
+}
+/**
+ * @desc Creates new PIREP
+ * @param {string} id - Unique ID of pirep
+ * @param {string} vehicle - Livery ID
+ * @param {string} vehiclePublic - Name of Vehicle
+ * @param {string} author - Pirep Creator
+ * @param {string} airline - Airline
+ * @param {string} depICAO - Departing ICAO code
+ * @param {string} arrICAO - Arriving ICAO code
+ * @param {string} route - Route
+ * @param {number} flightTime - Flight time
+ * @param {string} comments - Comments for flight
+ * @param {string} status - "N": Pending, "A": Approved, "D": Denided
+ * @param {number} fuel - Fuel used
+ * @param {string} filed - Time of creation
+ * @return Error
+ * @memberof PIREPS
+ */
+function CreatePirep(id, vehicle, vehiclePublic, author, airline, depICAO, arrICAO, route, flightTime, comments, status, fuel, filed) {
+    db.run(`INSERT INTO users(id, vehicle, vehiclePublic, author, airline, depICAO, arrICAO, route, flightTime, comments, status, fuel, filed) 
+            VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`, [id, vehicle, vehiclePublic, author, airline, depICAO, arrICAO, route, flightTime, comments, status, fuel, filed], function (err) {
+        return err;
+    });
+}
+/** @namespace Ranks */
+
+/** @namespace Routes */
+
+/** @namespace Slots */
+
+/** @namespace Tokens */
+
+/** @namespace Users */
+/**
+ * @desc Returns record of specific User ID (UID)
+ * @param {string} username - Unique username of user 
  * @returns {promise} Record for that username in an array
+ * @memberof Users
  */
 function GetUser(username) {
     return new Promise((resolve, error) => {
@@ -31,8 +111,8 @@ function GetUser(username) {
 
 /**
  * @desc Returns all users
- *
  * @returns {promise} All users in an array
+ * @memberof Users
  */
 function GetUsers() {
     return new Promise((resolve, error) => {
@@ -47,43 +127,30 @@ function GetUsers() {
     });
 }
 
-function CreateUser(username, rank, admin, password, display, hours, created, llogin, cp, revoked) {
+/**
+ * @desc Creates a new user
+ * @param {string} username - Digit Username
+ * @param {string} rank - Rank of user
+ * @param {boolean} admin - Admin status
+ * @param {string} password - Hashed password of user
+ * @param {string} display - Display Name
+ * @param {string} profileURL - URL to profile picture
+ * @param {number} hours - Flight Hours
+ * @param {string} created - Date and time created
+ * @param {string} llogin - Last logged in
+ * @param {boolean} cp - Force change password on next login
+ * @param {boolean} revoked - User access revoked
+ * @return Error
+ * @memberof Users
+ */
+function CreateUser(username, rank, admin, password, display, profileURL, hours, created, llogin, cp, revoked) {
     db.run(`INSERT INTO users(username, rank, admin, password, display, hours, created, llogin, cp, revoked) 
             VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`, [username, rank, admin, password, display, hours, created, llogin, cp, revoked], function (err) {
         return err;
     });
 }
 
-function GetPirep(id) {
-    return new Promise((resolve, error) => {
-        db.serialize(() => {
-            db.each(`SELECT * FROM pireps WHERE id = ?`, [id], (err, row) => {
-                if (err) {
-                    error(err.message);
-                }
-                resolve(row);
-            });
-        });
-    });
-}
 
-function GetPireps() {
-    return new Promise((resolve, error) => {
-        db.serialize(() => {
-            db.all(`SELECT * FROM pireps`, (err, rows) => {
-                if (err) {
-                    error(err.message);
-                }
-                resolve(rows);
-            });
-        });
-    });
-}
-
-function CreatePirep(username, rank, admin, password, display, hours, created, llogin, cp, revoked) {
-    db.run(`INSERT INTO users(username, rank, admin, password, display, hours, created, llogin, cp, revoked) 
-            VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`, [username, rank, admin, password, display, hours, created, llogin, cp, revoked], function (err) {
-        return err;
-    });
-}
-module.exports = { db, GetUser, GetUsers, CreateUser, GetPirep, GetPireps, CreatePirep };
+module.exports = { db,
+    GetUser, GetUsers, CreateUser,
+    GetPirep, GetPireps, CreatePirep };
