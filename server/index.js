f02e22dbbe2e2f209eac5b570945cc1eaac6be98
Sam-Neale <sammneale@icloud.com>
2021-09-28 22:04:39 +1000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -310,7 +310,7 @@ Sentry.init({
         // enable Express.js middleware tracing
         new Tracing.Integrations.Express({ app }),
     ],
-    environment: "Dev - Beta",
+    environment: "Production",
     debug: true,
     // Set tracesSampleRate to 1.0 to capture 100%
     // of transactions for performance monitoring.
